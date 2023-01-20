7fbc72527d99a5604b40e03c1527e41dcf93e4d7
kodi1993 <Akhmedka1993@gmail.com>
2023-01-20 17:01:29 +0300

    Add Footers and Headers

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import pandas as pd
 
 df = pd.read_csv('topics.csv')
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
+    # Add page, header and line
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(12, 40, 100)
-    page_header = row['Topic']
-    number_of_pages = int(row['Pages'])
-    pdf.cell(w=0, h=12, txt=page_header, align="L", ln=1)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
-    for page in range(1, number_of_pages):
+    # Add footer
+    pdf.ln(260)
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align="R", ln=1)
+    # Add empty pages with footers
+    for page in range(1, int(row['Pages'])):
         pdf.add_page()
+        pdf.ln(272)
         pdf.set_font(family="Times", size=10)
-        pdf.set_text_color(12, 40, 100)
-        page_header = row['Topic']
-        number_of_pages = int(row['Pages'])
-        pdf.cell(w=0, h=12, txt=page_header, align="R", ln=1)
-        pdf.line(180, 20, 200, 20)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row['Topic'], align="R", ln=1)
 
 pdf.output("new_pdf.pdf")
